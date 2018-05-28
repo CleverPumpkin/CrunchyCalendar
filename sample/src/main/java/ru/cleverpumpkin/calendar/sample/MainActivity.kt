45e0730f62eda3ea3de22c613de8ce71af0afd3f
Alexander Surinov <alexander.surinov@cleverpumpkin.ru>
2018-05-28 18:20:32 +0300

    Create project structure
    Add library module and sample app module.

---
+++ sample/src/main/java/ru/cleverpumpkin/calendar/sample/MainActivity.kt
@@ -0,0 +1,13 @@
+package ru.cleverpumpkin.calendar.sample
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import ru.cleverpumpkin.calendar.sample.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
