abd682380d62e2a12696da24883dc830e38abbd3
Alexander Surinov <alexander.surinov@cleverpumpkin.ru>
2018-06-07 14:28:23 +0300

    [LibAD-5] Custom calendar decoration
    Add sample with custom calendar events decoration.

--- sample/src/main/java/ru/cleverpumpkin/calendar/sample/MainActivity.kt
+++ sample/src/main/java/ru/cleverpumpkin/calendar/sample/MainActivity.kt
@@ -1,7 +1,9 @@
 package ru.cleverpumpkin.calendar.sample
 
+import android.graphics.Color
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import ru.cleverpumpkin.calendar.CalendarDate
 import ru.cleverpumpkin.calendar.CalendarView
 import java.util.*
 
@@ -11,25 +13,58 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val calendarView = findViewById<CalendarView>(R.id.calendar_view)
+
+        val calendar = Calendar.getInstance()
+        calendar.set(2018, Calendar.MAY, 2)
+        val minDate = calendar.time
+
+        calendar.set(2018, Calendar.JUNE, 28)
+        val maxDate = calendar.time
+
+        calendar.set(2018, Calendar.JUNE, 1)
+        val initialDate = calendar.time
+
         if (savedInstanceState == null) {
-            val calendarView = findViewById<CalendarView>(R.id.calendar_view)
-
-            val calendar = Calendar.getInstance()
-            calendar.set(2018, 5, 15)
-            val minDate = calendar.time
-
-            calendar.set(2018, 7, 16)
-            val maxDate = calendar.time
-
-            calendar.set(2018, 6, 1)
-            val initialDate = calendar.time
-
-            calendarView.setUp(
-                initialDate = initialDate,
-                minDate = minDate,
-                maxDate = maxDate,
-                selectionMode = CalendarView.SelectionMode.SINGLE
+            calendarView.setup(
+                initialDate = CalendarDate(initialDate),
+                minDate = null,
+                maxDate = null,
+                selectionMode = CalendarView.SelectionMode.MULTIPLE
             )
         }
+
+        val events = generateCalendarEvents()
+        val groupedEvents = events.groupBy { it.calendarDate }
+
+        val eventsDateItemDecoration = EventsDateItemDecoration(
+            context = this,
+            groupedEvents = groupedEvents
+        )
+
+        calendarView.addCustomItemDecoration(eventsDateItemDecoration)
+    }
+
+    private fun generateCalendarEvents(): List<CalendarEvent> {
+        val calendar = Calendar.getInstance()
+        calendar.set(2018, Calendar.JUNE, 15)
+
+        val event1 = CalendarEvent(
+            calendarDate = CalendarDate(calendar.time),
+            color = Color.RED
+        )
+
+        val event2 = CalendarEvent(
+            calendarDate = CalendarDate(calendar.time),
+            color = Color.GREEN
+        )
+
+        calendar.set(2018, Calendar.JUNE, 18)
+        val event3 = CalendarEvent(
+            calendarDate = CalendarDate(calendar.time),
+            color = Color.RED
+        )
+
+        return listOf(event1, event2, event3)
     }
 }
