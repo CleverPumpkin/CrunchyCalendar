0d0a6100ffbfa69ab3ae8704e521f02e237f73a0
Alexander Surinov <alexander.surinov@cleverpumpkin.ru>
2018-06-12 17:10:03 +0300

    [LibAD-12] Sample app
    Refactor sample app. Add base samples of calendar selection modes.

---
+++ sample/src/main/java/ru/cleverpumpkin/calendar/sample/DemoModeFragment.kt
@@ -0,0 +1,53 @@
+package ru.cleverpumpkin.calendar.sample
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v7.widget.Toolbar
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import ru.cleverpumpkin.calendar.CalendarView
+import ru.cleverpumpkin.calendar.sample.DemoModeListFragment.DemoMode
+import java.lang.IllegalStateException
+
+class DemoModeFragment : Fragment() {
+
+    companion object {
+        private const val ARG_DEMO_MODE = "ru.cleverpumpkin.calendar.sample.demo_mode"
+
+        fun newInstance(demoMode: DemoMode): Fragment {
+            return DemoModeFragment().apply {
+                arguments = Bundle().apply { putString(ARG_DEMO_MODE, demoMode.name) }
+            }
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_demo_mode, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val demoModeName = arguments?.getString(ARG_DEMO_MODE)
+                ?: throw IllegalStateException()
+
+        val demoMode = DemoMode.valueOf(demoModeName)
+
+        val toolbar = view.findViewById<Toolbar>(R.id.toolbar)
+        toolbar.run {
+            setTitle(demoMode.desctiption)
+            setNavigationIcon(R.drawable.ic_arrow_back_24dp)
+            setNavigationOnClickListener { activity?.onBackPressed() }
+        }
+
+        val calendarView = view.findViewById<CalendarView>(R.id.calendar_view)
+        if (savedInstanceState == null) {
+            calendarView.setupCalendar(selectionMode = demoMode.selectionMode)
+        }
+    }
+}
--- sample/src/main/java/ru/cleverpumpkin/calendar/sample/MainActivity.kt
+++ sample/src/main/java/ru/cleverpumpkin/calendar/sample/MainActivity.kt
@@ -2,69 +2,31 @@ package ru.cleverpumpkin.calendar.sample
 
 import android.graphics.Color
 import android.os.Bundle
+import android.support.v4.app.FragmentTransaction
+
 import android.support.v7.app.AppCompatActivity
 import ru.cleverpumpkin.calendar.CalendarDate
-import ru.cleverpumpkin.calendar.CalendarView
+import ru.cleverpumpkin.calendar.sample.DemoModeListFragment.DemoMode
 import java.util.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), DemoModeListFragment.OnDemoModeClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val calendarView = findViewById<CalendarView>(R.id.calendar_view)
-
-        val calendar = Calendar.getInstance()
-        calendar.set(2018, Calendar.MAY, 2)
-        val minDate = calendar.time
-
-        calendar.set(2018, Calendar.JUNE, 28)
-        val maxDate = calendar.time
-
-        calendar.set(2018, Calendar.JUNE, 1)
-        val initialDate = calendar.time
-
         if (savedInstanceState == null) {
-            calendarView.setup(
-                initialDate = CalendarDate(initialDate),
-                minDate = null,
-                maxDate = null,
-                selectionMode = CalendarView.SelectionMode.MULTIPLE
-            )
+            supportFragmentManager.beginTransaction()
+                .add(R.id.fragment_container, DemoModeListFragment())
+                .commit()
         }
-
-        val events = generateCalendarEvents()
-        val groupedEvents = events.groupBy { it.calendarDate }
-
-        val eventsDateItemDecoration = EventsDateItemDecoration(
-            context = this,
-            groupedEvents = groupedEvents
-        )
-
-        calendarView.addCustomItemDecoration(eventsDateItemDecoration)
     }
 
-    private fun generateCalendarEvents(): List<CalendarEvent> {
-        val calendar = Calendar.getInstance()
-        calendar.set(2018, Calendar.JUNE, 15)
-
-        val event1 = CalendarEvent(
-            calendarDate = CalendarDate(calendar.time),
-            color = Color.RED
-        )
-
-        val event2 = CalendarEvent(
-            calendarDate = CalendarDate(calendar.time),
-            color = Color.GREEN
-        )
-
-        calendar.set(2018, Calendar.JUNE, 18)
-        val event3 = CalendarEvent(
-            calendarDate = CalendarDate(calendar.time),
-            color = Color.RED
-        )
-
-        return listOf(event1, event2, event3)
+    override fun onDemoModeClick(demoMode: DemoMode) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, DemoModeFragment.newInstance(demoMode))
+            .addToBackStack(null)
+            .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+            .commit()
     }
 }
