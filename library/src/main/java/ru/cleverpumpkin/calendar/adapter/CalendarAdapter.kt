ebcc3124379fc5bb921953364c3b13901f72b82a
Alexander Surinov <alexander.surinov@cleverpumpkin.ru>
2018-06-18 11:09:18 +0300

    Update CalendarAdapter doc comment

--- library/src/main/java/ru/cleverpumpkin/calendar/adapter/CalendarAdapter.kt
+++ library/src/main/java/ru/cleverpumpkin/calendar/adapter/CalendarAdapter.kt
@@ -28,14 +28,16 @@ import java.util.*
 /**
  * This class provides view items for calendar.
  *
- * This adapter provides three types of elements: [DATE_VIEW_TYPE] - single date cell,
- * [MONTH_VIEW_TYPE] - simple name of month, [EMPTY_VIEW_TYPE] - empty view that represents
- * start and end offset for each month.
+ * This adapter provides three types of elements:
+ * [DATE_VIEW_TYPE] - single date cell
+ * [MONTH_VIEW_TYPE] - simple name of month
+ * [EMPTY_VIEW_TYPE] - empty view that represents start and end offset for each month
  */
 class CalendarAdapter(
     var itemsAttributes: ItemsAttributes,
     private val dateInfoProvider: CalendarView.DateInfoProvider,
     private val onDateClickHandler: (CalendarDate) -> Unit
+
 ) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
     companion object {
