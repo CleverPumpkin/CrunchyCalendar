5f046142b7d63958a981ba6eb1db072f70caae74
Alexander Surinov <alexander.surinov@cleverpumpkin.ru>
2018-09-03 20:05:10 +0300

    Unit testing
    Add set of unit tests for a RangeDateSelectionStrategy class.
    Small refactoring

--- crunchycalendar/src/test/java/ru/cleverpumpkin/calendar/selection/TestData.kt
+++ crunchycalendar/src/test/java/ru/cleverpumpkin/calendar/selection/TestData.kt
@@ -11,4 +11,17 @@ object TestData {
             calendar.set(2018, Calendar.JANUARY, 1)
             return CalendarDate(calendar.time)
         }
+
+    fun generateDatesBetween(dateFrom: CalendarDate, dateTo: CalendarDate): List<CalendarDate> {
+        val dates = mutableListOf<CalendarDate>()
+        val daysBetween = dateFrom.daysBetween(dateTo)
+        val calendar = dateFrom.calendar
+
+        repeat(daysBetween.inc()) {
+            dates += CalendarDate(calendar.time)
+            calendar.add(Calendar.DAY_OF_MONTH, 1)
+        }
+
+        return dates
+    }
 }
